a14f0c919385ea7e3f77a61ebfd9d016b3dfbb3e
Yadukrishnan_K_M <ee24mt013@iitdh.ac.in>
2024-11-14 16:39:40 +0530

    sd card checking codes for proper operation

--- Group_06_Project_SD_card_Data_logger/main.c
+++ Group_06_Project_SD_card_Data_logger/main.c
@@ -6,8 +6,11 @@
 #define RED_LED    (1U << 1) // PF1
 #define BLUE_LED   (1U << 2) // PF2
 #define GREEN_LED  (1U << 3) // PF3
+#define SD_CS_Pin  (1U << 3) // PA3
 
 // SPI(0) pin connections: PA2:-SCK(0) PA3:-CS(0) PA4:-MISO(0) PA5:-MOSI(0)
+
+void SD_Card_Present(void);
 
 //Interrupt Handlers
 
@@ -18,7 +21,8 @@ void GPIO_PortF_Interrupt_Handler(void)
     }
 
     if(GPIO_PORTF_MIS_R == 0x01){           // Interrupt from SW2
-        GPIO_PORTF_DATA_R ^= RED_LED;       // Toggle data in PF1 (red led)
+        //GPIO_PORTF_DATA_R ^= RED_LED;       // Toggle data in PF1 (red led)
+        SD_Card_Present();
     }
 
     GPIO_PORTF_ICR_R = 0x11;                // Interrupt clear, 1-clear all prior interrupts (PF7-PF0 = 00010001)
@@ -80,9 +84,52 @@ void SPI_0_Initialisation(void){
     SSI0_CR1_R = 0x00;                      // SPI control, disable fully
     SSI0_CC_R = 0x00;                       // Clock configuration - system clock
     SSI0_CPSR_R = 0x02;                     // Clock divisor pre-scalar, 2
-    SSI0_CR0_R = 0x07c7;                    // SPI settings, 16 bit-all zero(reserved), 8 bit-0x07(serial clock rate 1Mbps), SPH-1, SPO-1, 2 bit-0(mode freescale), 4 bit-7(8bit data)
+    SSI0_CR0_R = (0x07 << 8) | (0x02 << 6); // 8-bit data, Freescale mode
+    //SSI0_CR0_R = 0x07c7;                    // SPI settings, 16 bit-all zero(reserved), 8 bit-0x07(serial clock rate 1Mbps), SPH-1, SPO-1, 2 bit-0(mode freescale), 4 bit-7(8bit data)
     SSI0_CR1_R = 0x02;                      // SPI control, 00010 - endTxInterrupt, reserved, MasterOperation, SPIEnable, LoopbackDisable
 
+}
+
+void SPI_Transmit(uint8_t data) {
+
+    while ((SSI0_SR_R & SSI_SR_TNF) == 0) GPIO_PORTF_DATA_R |= RED_LED;   // Wait until there's space in FIFO
+    SSI0_DR_R = data;                       // Send data
+    GPIO_PORTF_DATA_R &= ~RED_LED;
+}
+
+uint8_t SPI_Receive(void) {
+
+    while ((SSI0_SR_R & SSI_SR_RNE) == 0) GPIO_PORTF_DATA_R |= GREEN_LED;  // Wait until there's data to read
+    GPIO_PORTF_DATA_R &= ~GREEN_LED;
+    return SSI0_DR_R;                       // Read and return data
+
+}
+
+void SD_Card_Present(void) {
+    GPIO_PORTF_DATA_R |= BLUE_LED;
+    uint8_t response = 2;
+
+    // Pull CS low to select the SD card
+    GPIO_PORTA_DATA_R &= ~SD_CS_Pin;  // Activate the chip select
+
+    // Send CMD0 (GO_IDLE_STATE) to check for presence
+    SPI_Transmit(0x40 | 0x00);       // CMD0 with start bit
+    SPI_Transmit(0x00);               // Argument (0)
+    SPI_Transmit(0x95);               // CRC for CMD0 (valid CRC)
+
+    // Wait for response (R1 format)
+    response = SPI_Receive();         // Read response
+
+    // Release CS
+    GPIO_PORTA_DATA_R |= SD_CS_Pin;      // Deactivate the chip select
+    GPIO_PORTF_DATA_R &= ~BLUE_LED;
+
+    // Check if the response is valid (R1 response is 0)
+    if (response == 0x01) {
+        GPIO_PORTF_DATA_R |= BLUE_LED;                    // Turn on LED if reset was successful
+    } else {
+        GPIO_PORTF_DATA_R &= ~BLUE_LED;                   // Turn off LED if failed
+    }
 }
 
 void main(void){
