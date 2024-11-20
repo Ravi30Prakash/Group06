3263ac3bf4287ef386ed86c55f6296dfd5466fac
Ravi30Prakash <raviprakash30nov@gmail.com>
2024-11-20 18:17:16 +0530

    SPI_2, GPIO A&F_Init, SD Card

--- Group_06_Project_SD_card_Data_logger/initialisation.c
+++ Group_06_Project_SD_card_Data_logger/initialisation.c
@@ -3,4 +3,85 @@
 #include "tm4c123gh6pm.h"
 #include "definitions.h"
 
+void GPIO_PortA_Initialisation(void){
+
+    //volatile unsigned long delay;
+    // SPI(0) pin connections: PA2:-SCK(0) PA3:-CS(0) PA4:-MISO(0) PA5:-MOSI(0)
+
+    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R0;// Enable clock for Port A
+    //SYSCTL_RCGC2_R |= 0x00000001;         // activate clock for Port A
+    //delay = SYSCTL_RCGC2_R;               // allow time for clock to stabilize
+    GPIO_PORTA_LOCK_R = GPIO_LOCK_KEY;      // Unlock Port A
+    GPIO_PORTA_CR_R = 0x3c;                 // Commit changes,1-enable (PA7-PA0 = 00111100)
+    GPIO_PORTA_DEN_R = 0x3c;                // Digital function enable, 1-enable (PA7-PA0 = 00111100)
+    GPIO_PORTA_DIR_R = 0x2c;                // Set output/input, 1-output (PA7-PA0 = 00101100)
+    GPIO_PORTA_PUR_R = 0x0c;                // Enable pull-up resistor, 1-enable (PA7-PA0 = 00001100)
+    GPIO_PORTA_DATA_R = 0x00;               // Reset the data register (PA7-PA0 = 00000000)
+
+}
+
+void GPIO_PortB_Initialisation(void){
+
+    //volatile unsigned long delay;
+    // SPI(2) pin connections: PB4:-SCK(2) PB5:-CS(2) PB6:-MISO(2) PB7:-MOSI(2)
+
+    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R1;// Enable clock for Port B
+    //SYSCTL_RCGC2_R |= 0x00000010;         // activate clock for Port B
+    //delay = SYSCTL_RCGC2_R;               // allow time for clock to stabilize
+    GPIO_PORTB_LOCK_R = GPIO_LOCK_KEY;      // Unlock Port B
+    GPIO_PORTB_CR_R = 0xf0;                 // Commit changes,1-enable (PB7-PB0 = 11110000)
+    GPIO_PORTB_DEN_R = 0xf0;                // Digital function enable, 1-enable (PB7-PB0 =11110000)
+    GPIO_PORTB_DIR_R = 0xb0;                // Set output/input, 1-output (PB7-PB0 = 10110000)
+    GPIO_PORTB_PUR_R = 0x30;                // Enable pull-up resistor, 1-enable (PB7-PB0 = 00110000)
+    GPIO_PORTB_DATA_R = 0x00;               // Reset the data register (PB7-PB0 = 00000000)
+
+}
+
+void GPIO_PortF_Initialisation(void){
+
+    // PORTF, PF7-PF0, PF4-SW1, PF3-green, PF2-blue, PF1-red, PF0-SW2
+
+    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R5;// Enable clock for Port F
+    GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;      // Unlock Port F
+    GPIO_PORTF_CR_R = 0x1f;                 // Commit changes,1-enable (PF7-PF0 = 00011111)
+    GPIO_PORTF_DEN_R = 0x1f;                // Digital function enable, 1-enable (PF7-PF0 = 00011111)
+    GPIO_PORTF_DIR_R = 0x0e;                // Set output/input, 1-output (PF7-PF0 = 00001110)
+    GPIO_PORTF_PUR_R = 0x11;                // Enable pull-up resistor, 1-enable (PF7-PF0 = 00010001)
+    GPIO_PORTF_DATA_R = 0x00;               // Reset the data register (PF7-PF0 = 00000000)
+
+}
+
+// SPI(0) pin connections: PA2:-SCK(0) PA3:-CS(0) PA4:-MISO(0) PA5:-MOSI(0)
+
+void SPI_0_Initialisation(void){
+
+    SYSCTL_RCGCSSI_R |= SYSCTL_RCGCSSI_R0;  // Enable clock for SPI(0) module
+    GPIO_PORTA_AFSEL_R = 0x3c;              // Alternate function,1-enable (PA7-PA0 = 00111100)
+    GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R & 0xff0000ff) | 0x00222200; // Set PA2, PA3, PA4 and PA5 to SPI (0)
+    SSI0_CR1_R = 0x00;                      // SPI control, disable fully
+    SSI0_CC_R = 0x00;                       // Clock configuration - system clock
+    SSI0_CPSR_R = 0x02;                     // Clock divisor pre-scalar, 2
+    SSI0_CR0_R = (0x07 << 8) | (0x02 << 6); // 8-bit data, Freescale mode
+    //SSI0_CR0_R = 0x07c7;                    // SPI settings, 16 bit-all zero(reserved), 8 bit-0x07(serial clock rate 1Mbps), SPH-1, SPO-1, 2 bit-0(mode freescale), 4 bit-7(8bit data)
+    SSI0_CR1_R = 0x02;                      // SPI control, 00010 - endTxInterrupt, reserved, MasterOperation, SPIEnable, LoopbackDisable
+    //modify();
+
+}
+
+// SPI(2) pin connections: PB4:-SCK(2) PB5:-CS(2) PB6:-MISO(2) PB7:-MOSI(2)
+
+void SPI_2_Initialisation(void){
+
+    SYSCTL_RCGCSSI_R |= SYSCTL_RCGCSSI_R2;  // Enable clock for SPI(2) module
+    GPIO_PORTB_AFSEL_R = 0xf0;              // Alternate function,1-enable (PB7-PB0 = 11110000)
+    GPIO_PORTB_PCTL_R = (GPIO_PORTB_PCTL_R & 0x0000ffff) | 0x22220000; // Set PB4, PB5, PB6 and PB7 to SPI (2)
+    SSI2_CR1_R = 0x00;                      // SPI control, disable fully
+    SSI2_CC_R = 0x00;                       // Clock configuration - system clock
+    SSI2_CPSR_R = 0x02;                     // Clock divisor pre-scalar, 2
+    SSI2_CR0_R = (0x07 << 8) | (0x02 << 6); // 8-bit data, Freescale mode
+    //SSI2_CR0_R = 0x07c7;                   // SPI settings, 16 bit-all zero(reserved), 8 bit-0x07(serial clock rate 1Mbps), SPH-1, SPO-1, 2 bit-0(mode freescale), 4 bit-7(8bit data)
+    SSI2_CR1_R = 0x02;                      // SPI control, 00010 - endTxInterrupt, reserved, MasterOperation, SPIEnable, LoopbackDisable
+    //modify();
+
+}
 
--- Group_06_Project_SD_card_Data_logger/main.c
+++ Group_06_Project_SD_card_Data_logger/main.c
@@ -1,9 +1,5 @@
-//red led: stuck in tx while loop
-//green led: stuck in rx while loop
-//bue led: sd card status and working branch indication
 
 // SPI(0) pin connections: PA2:-SCK(0) PA3:-CS(0) PA4:-MISO(0) PA5:-MOSI(0)
-// SPI(2) pin connections: PB4:-SCK(2) PB5:-CS(2) PB6:-MISO(2) PB7:-MOSI(2)
 
 // ********************************************///////////****************************************************//
 
@@ -11,105 +7,25 @@
 #include <stdbool.h>
 #include "tm4c123gh6pm.h"
 #include "definitions.h"
-<<<<<<< HEAD
 
-// Define LED pins
-#define RED_LED    (1U << 1) // PF1
-#define BLUE_LED   (1U << 2) // PF2
-#define GREEN_LED  (1U << 3) // PF3
-#define SD_CS_Pin  (1U << 3) // PA3
+// *******************************************////////////***************************************************//
 
-// SPI(0) pin connections: PA2:-SCK(0) PA3:-CS(0) PA4:-MISO(0) PA5:-MOSI(0)
+void main(void){
 
-void SD_Card_Present(void);
+    SysTick_Initialisation();
+    GPIO_PortF_Initialisation();
+    GPIO_PortF_Interrupt_Initialisation();
+    GPIO_PortA_Initialisation();
+    //modify();
+    SPI_0_Initialisation();
 
-//Interrupt Handlers
+    while(1){
 
-void GPIO_PortF_Interrupt_Handler(void)
-{
-    if(GPIO_PORTF_MIS_R == 0x10){           // Interrupt from SW1
-        GPIO_PORTF_DATA_R ^= GREEN_LED;     // Toggle data in PF1 (green led)
     }
 
-    if(GPIO_PORTF_MIS_R == 0x01){           // Interrupt from SW2
-        //GPIO_PORTF_DATA_R ^= RED_LED;       // Toggle data in PF1 (red led)
-        SD_Card_Present();
-    }
-
-    GPIO_PORTF_ICR_R = 0x11;                // Interrupt clear, 1-clear all prior interrupts (PF7-PF0 = 00010001)
 }
 
-// Other functions
-
-void GPIO_PortF_Interrupt_Initialisation(void){
-
-     // Setting PORTF interrupt registers
-     GPIO_PORTF_IS_R = 0x00;                // Interrupt sense, 1-level, 0-edge (PF7-PF0 = 00000000)
-     GPIO_PORTF_IBE_R = 0x00;               // Interrupt both edge, 1-both edge, 0-single edge (PF7-PF0 = 00000000)
-     GPIO_PORTF_IEV_R = 0x00;               // Interrupt event, 0-falling edge, 1-falling edge (PF7-PF0 = 00000000)
-     GPIO_PORTF_ICR_R = 0xff;               // Interrupt clear, 1-clear all prior interrupts (PF7-PF0 = 11111111)
-     GPIO_PORTF_IM_R = 0x11;                // Interrupt mask, 1-Enable interrupt (PF7-PF0 = 00010001)
-
-     NVIC_PRI7_R &= 0xFF3FFFFF;             // Prioritize and enable interrupts in NVIC
-
-     // Enable the GPIO Port F interrupt in the NVIC
-     NVIC_EN0_R = 0x40000000;               // Port F is interrupt 23 in the NVIC port is in 31st position
-
-     //__asm("    cpsie i");                // Global interrupt enable
-
-}
-
-// Interrupt Handlers
-
-void GPIO_PortF_Interrupt_Handler(void)
-{
-    if(GPIO_PORTF_MIS_R == 0x10){           // Interrupt from SW1
-        GPIO_PORTF_DATA_R ^= GREEN_LED;     // Toggle data in PF1 (green led)
-    }
-
-    if(GPIO_PORTF_MIS_R == 0x01){           // Interrupt from SW2
-        GPIO_PORTF_DATA_R ^= RED_LED;       // Toggle data in PF1 (red led)
-        SD_Card_Present();
-    }
-
-    GPIO_PORTF_ICR_R = 0x11;                // Interrupt clear, 1-clear all prior interrupts (PF7-PF0 = 00010001)
-}
-
-
-void GPIO_PortF_Interrupt_Initialisation(void){
-
-     // Setting PORTF interrupt registers
-     GPIO_PORTF_IS_R = 0x00;                // Interrupt sense, 1-level, 0-edge (PF7-PF0 = 00000000)
-     GPIO_PORTF_IBE_R = 0x00;               // Interrupt both edge, 1-both edge, 0-single edge (PF7-PF0 = 00000000)
-     GPIO_PORTF_IEV_R = 0x00;               // Interrupt event, 0-falling edge, 1-falling edge (PF7-PF0 = 00000000)
-     GPIO_PORTF_ICR_R = 0xff;               // Interrupt clear, 1-clear all prior interrupts (PF7-PF0 = 11111111)
-     GPIO_PORTF_IM_R = 0x11;                // Interrupt mask, 1-Enable interrupt (PF7-PF0 = 00010001)
-
-     NVIC_PRI7_R &= 0xFF3FFFFF;             // Prioritize and enable interrupts in NVIC
-
-     // Enable the GPIO Port F interrupt in the NVIC
-     NVIC_EN0_R = 0x40000000;               // Port F is interrupt 23 in the NVIC port is in 31st position
-
-     //__asm("    cpsie i");                // Global interrupt enable
-
-}
-
-void GPIO_PortA_Initialisation(void){
-
-    //volatile unsigned long delay;
-    // SPI(0) pin connections: PA2:-SCK(0) PA3:-CS(0) PA4:-MISO(0) PA5:-MOSI(0)
-
-    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R0;// Enable clock for Port A
-    //SYSCTL_RCGC2_R |= 0x00000001;         // activate clock for Port A
-    //delay = SYSCTL_RCGC2_R;               // allow time for clock to stabilize
-    GPIO_PORTA_LOCK_R = GPIO_LOCK_KEY;      // Unlock Port A
-    GPIO_PORTA_CR_R = 0x3c;                 // Commit changes,1-enable (PA7-PA0 = 00111100)
-    GPIO_PORTA_DEN_R = 0x3c;                // Digital function enable, 1-enable (PA7-PA0 = 00111100)
-    GPIO_PORTA_DIR_R = 0x2c;                // Set output/input, 1-output (PA7-PA0 = 00101100)
-    GPIO_PORTA_PUR_R = 0x0c;                // Enable pull-up resistor, 1-enable (PA7-PA0 = 00001100)
-    GPIO_PORTA_DATA_R = 0x00;               // Reset the data register (PA7-PA0 = 00000000)
-
-}
+// ********************************************///////////****************************************************//
 
 void modify(void){
     GPIO_PORTA_AMSEL_R &= ~0xFC;          // disable analog functionality on PA2,3,4,5
@@ -125,86 +41,6 @@ void modify(void){
                                             // DSS = 8-bit data
     SSI0_CR0_R = (SSI0_CR0_R&~SSI_CR0_DSS_M)+SSI_CR0_DSS_8;
     SSI0_CR1_R|=SSI_CR1_SSE;          // 3)Enable SSI
-}
-
-
-void SPI_0_Initialisation(void){
-
-    SYSCTL_RCGCSSI_R |= SYSCTL_RCGCSSI_R0;  // Enable clock for SPI(0) module
-    GPIO_PORTA_AFSEL_R = 0x3c;              // Alternate function,1-enable (PA7-PA0 = 00111100)
-    GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R & 0xff0000ff) | 0x00222200; // Set PA2, PA3, PA4 and PA5 to SPI (0)
-    SSI0_CR1_R = 0x00;                      // SPI control, disable fully
-    SSI0_CC_R = 0x00;                       // Clock configuration - system clock
-    SSI0_CPSR_R = 0x02;                     // Clock divisor pre-scalar, 2
-    SSI0_CR0_R = (0x07 << 8) | (0x02 << 6); // 8-bit data, Freescale mode
-    //SSI0_CR0_R = 0x07c7;                    // SPI settings, 16 bit-all zero(reserved), 8 bit-0x07(serial clock rate 1Mbps), SPH-1, SPO-1, 2 bit-0(mode freescale), 4 bit-7(8bit data)
-    SSI0_CR1_R = 0x02;                      // SPI control, 00010 - endTxInterrupt, reserved, MasterOperation, SPIEnable, LoopbackDisable
-
-}
-
-void SPI_2_Initialisation(void){
-
-    SYSCTL_RCGCSSI_R |= SYSCTL_RCGCSSI_R2;  // Enable clock for SPI(2) module
-    GPIO_PORTB_AFSEL_R = 0xf0;              // Alternate function,1-enable (PB7-PB0 = 11110000)
-    GPIO_PORTB_PCTL_R = (GPIO_PORTB_PCTL_R & 0x0000ffff) | 0x22220000; // Set PB4, PB5, PB6 and PB7 to SPI (2)
-    SSI2_CR1_R = 0x00;                      // SPI control, disable fully
-    SSI2_CC_R = 0x00;                       // Clock configuration - system clock
-    SSI2_CPSR_R = 0x02;                     // Clock divisor pre-scalar, 2
-    SSI2_CR0_R = (0x07 << 8) | (0x02 << 6); // 8-bit data, Freescale mode
-    //SSI2_CR0_R = 0x07c7;                   // SPI settings, 16 bit-all zero(reserved), 8 bit-0x07(serial clock rate 1Mbps), SPH-1, SPO-1, 2 bit-0(mode freescale), 4 bit-7(8bit data)
-    SSI2_CR1_R = 0x02;                      // SPI control, 00010 - endTxInterrupt, reserved, MasterOperation, SPIEnable, LoopbackDisable
-    //modify();
-
-}
-
-void GPIO_PortB_Initialisation(void){
-
-    //volatile unsigned long delay;
-    // SPI(2) pin connections: PB4:-SCK(2) PB5:-CS(2) PB6:-MISO(2) PB7:-MOSI(2)
-
-    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R1;// Enable clock for Port B
-    //SYSCTL_RCGC2_R |= 0x00000010;         // activate clock for Port B
-    //delay = SYSCTL_RCGC2_R;               // allow time for clock to stabilize
-    GPIO_PORTB_LOCK_R = GPIO_LOCK_KEY;      // Unlock Port B
-    GPIO_PORTB_CR_R = 0xf0;                 // Commit changes,1-enable (PB7-PB0 = 11110000)
-    GPIO_PORTB_DEN_R = 0xf0;                // Digital function enable, 1-enable (PB7-PB0 =11110000)
-    GPIO_PORTB_DIR_R = 0xb0;                // Set output/input, 1-output (PB7-PB0 = 10110000)
-    GPIO_PORTB_PUR_R = 0x30;                // Enable pull-up resistor, 1-enable (PB7-PB0 = 00110000)
-    GPIO_PORTB_DATA_R = 0x00;               // Reset the data register (PB7-PB0 = 00000000)
-
-}
-
-
-void SysTick_Delay_1mS(unsigned long delay){ // no of 1 mS
-  unsigned long i;
-  for(i=0; i<delay; i++){
-      while((NVIC_ST_CTRL_R&0x00010000)==0){ // wait for count flag
-      }
-  }
-}
-
-void SysTick_Initialisation(void){
-  NVIC_ST_CTRL_R = 0;               // disable SysTick during setup
-  NVIC_ST_RELOAD_R = 15999;         // number of counts to wait, 1 mS
-  NVIC_ST_CURRENT_R = 0;            // clears current value register
-  NVIC_ST_CTRL_R = 0x00000005;      // enable SysTick with core clock
-}
-
-
-
-void SPI_Transmit(uint8_t data) {
-
-    while ((SSI0_SR_R & SSI_SR_TNF) == 0) GPIO_PORTF_DATA_R |= RED_LED;   // Wait until there's space in FIFO
-    SSI0_DR_R = data;                       // Send data
-    GPIO_PORTF_DATA_R &= ~RED_LED;
-}
-
-uint8_t SPI_Receive(void) {
-
-    while ((SSI0_SR_R & SSI_SR_RNE) == 0) GPIO_PORTF_DATA_R |= GREEN_LED;  // Wait until there's data to read
-    GPIO_PORTF_DATA_R &= ~GREEN_LED;
-    return SSI0_DR_R;                       // Read and return data
-
 }
 
 void SD_Card_Present(void) {
@@ -237,112 +73,3 @@ void SD_Card_Present(void) {
         GPIO_PORTF_DATA_R &= ~BLUE_LED;                   // Turn off LED if failed
     }
 }
-=======
-uint8_t response;
-extern bool button_Send;
-
-// *******************************************////////////***************************************************//
->>>>>>> 2178b1a89d5b4ffb02b53533c988585cb782980c
-
-void main(void){
-
-    SysTick_Initialisation();
-    GPIO_PortF_Initialisation();
-    GPIO_PortF_Interrupt_Initialisation();
-    GPIO_PortA_Initialisation();
-    GPIO_PortB_Initialisation();
-    SPI_2_Initialisation();
-    UART0_Initialisation();
-    UART0_Println("Hello");
-    UART0_Println("Initialisation done");
-
-    while(1){
-
-        if(button_Send) {
-            UART0_Println("SD sent button pressed");
-            SD_Card_Present();
-            button_Send = 0;                    // To disable the flag for start SD data sending
-            UART0_Println("exiting SD sent button pressed loop");
-        }
-
-    }
-
-}
-
-// ********************************************///////////****************************************************//
-
-void SD_Card_Present(void) {
-
-    //uint8_t response;
-
-    SD_Initial_Clock_Pulses();
-
-    UART0_Println("**************************************Sending CMD0");
-
-    response = SD_Send(CMD0, Zero_Arg, dummy_CRC);
-    SysTick_Delay_1mS(100);
-
-    if(response == 0x01) UART0_Println("CMD0 Success");
-    else {
-        UART0_Println("CMD0 fail, 5s wait");
-        SysTick_Delay_1mS(5000);
-    }
-
- /*   UART0_Println("*****************************************Sending CMD8");
-
-    response = SD_Send(CMD8, CMD8_Arg, CMD8_CRC);
-    SysTick_Delay_1mS(100);
-
-    if(response == 0x01) UART0_Println("CMD8 Success");
-    else {
-        UART0_Println("CMD8 fail, 5s wait");
-        SysTick_Delay_1mS(5000);
-    }  */
-
-    UART0_Println("******************************************Sending CMD0");
-
-    response = SD_Send(CMD0, Zero_Arg, dummy_CRC);
-    SysTick_Delay_1mS(100);
-
-    if(response == 0x01) UART0_Println("CMD0 Success");
-    else {
-        UART0_Println("CMD0 fail, 5s wait");
-        SysTick_Delay_1mS(5000);
-    }
-
-    UART0_Println("****************************************Sending CMD55");
-
-    response = SD_Send(CMD55, Zero_Arg, dummy_CRC);
-    SysTick_Delay_1mS(100);
-
-    if(response == 0x00) UART0_Println("CMD55 Success");
-    else {
-        UART0_Println("CMD55 fail, 5s wait");
-        SysTick_Delay_1mS(5000);
-    }
-
-    UART0_Println("****************************************Sending CMD55");
-
-    response = SD_Send(CMD55, Zero_Arg, dummy_CRC);
-    SysTick_Delay_1mS(100);
-
-    if(response == 0x00) UART0_Println("CMD55 Success");
-    else {
-        UART0_Println("CMD55 fail, 5s wait");
-        SysTick_Delay_1mS(5000);
-    }
-
-
-    UART0_Println("***************************************Sending CMD41");
-
-    response = SD_Send(CMD41, CMD41_Arg, dummy_CRC);
-    SysTick_Delay_1mS(100);
-
-    if(response == 0x00) UART0_Println("CMD41 Success");
-    else {
-        UART0_Println("CMD41 fail, 5s wait");
-        SysTick_Delay_1mS(5000);
-    }
-
-}
-
