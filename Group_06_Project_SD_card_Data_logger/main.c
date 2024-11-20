cfc9284cb536ad74cb335d3447c83255756dd275
Yadukrishnan_K_M <ee24mt013@iitdh.ac.in>
2024-11-20 20:15:42 +0530

    SD init fuction testing

--- Group_06_Project_SD_card_Data_logger/main.c
+++ Group_06_Project_SD_card_Data_logger/main.c
@@ -11,8 +11,10 @@
 #include <stdbool.h>
 #include "tm4c123gh6pm.h"
 #include "definitions.h"
+
 uint8_t response;
 extern bool button_Send;
+uint8_t ocr[4];            // The OCR register value
 
 // *******************************************////////////***************************************************//
 
@@ -32,7 +34,7 @@ void main(void){
 
         if(button_Send) {
             UART0_Println("SD sent button pressed");
-            SD_Card_Present();
+            SD_Card_Initialisation();
             button_Send = 0;                    // To disable the flag for start SD data sending
             UART0_Println("exiting SD sent button pressed loop");
         }
@@ -43,77 +45,65 @@ void main(void){
 
 // ********************************************///////////****************************************************//
 
-void SD_Card_Present(void) {
+bool SD_Card_Initialisation(void){
 
-    //uint8_t response;
-
+    UART0_Println("SD_card_init... Starting...");
+    UART0_Println("Initial clk pulse sequence starting...");
     SD_Initial_Clock_Pulses();
+    UART0_Println("80 clk pulses send");
 
-    UART0_Println("**************************************Sending CMD0");
+    UART0_Println("Sending CMD0..........");
+    if(SD_Send(CMD0, Zero_Arg, dummy_CRC) == 1){
 
-    response = SD_Send(CMD0, Zero_Arg, dummy_CRC);
-    SysTick_Delay_1mS(100);
+        UART0_Println("CMD0 Success!");
+        UART0_Println("Sending CMD8..........");
+        SysTick_Delay_1mS(10);
 
-    if(response == 0x01) UART0_Println("CMD0 Success");
-    else {
-        UART0_Println("CMD0 fail, 5s wait");
-        SysTick_Delay_1mS(5000);
+        if(SD_Send(CMD8, CMD8_Arg, CMD8_CRC) == 1){
+
+            UART0_Println("CMD8 Success!");
+
+            int i;
+            for(i=0; i<4; i++){                                    //  Reading OCR register value
+                ocr[i]=SPI2_Transmit(0xff);
+            }
+
+            UART0_Println("Sending CMD55..........");
+            SysTick_Delay_1mS(10);
+
+            if(SD_Send(CMD55, Zero_Arg, dummy_CRC) <= 1){
+
+                UART0_Println("CMD55 Success!");
+                UART0_Println("Sending CMD41..........");
+                SysTick_Delay_1mS(100);
+
+                response = SD_Send(CMD41, CMD41_Arg, 0x77);
+                if(response == 0){
+
+                    UART0_Println("CMD41 Success!");
+                    return 1;
+                }
+                else {
+                    UART0_Println("CMD41 failed");
+                    return 0;
+                }
+
+            }
+            else {
+                UART0_Println("CMD55 failed");
+                return 0;
+            }
+
+        }
+        else {
+            UART0_Println("CMD8 failed");
+            return 0;
+        }
+
     }
-
- /*   UART0_Println("*****************************************Sending CMD8");
-
-    response = SD_Send(CMD8, CMD8_Arg, CMD8_CRC);
-    SysTick_Delay_1mS(100);
-
-    if(response == 0x01) UART0_Println("CMD8 Success");
     else {
-        UART0_Println("CMD8 fail, 5s wait");
-        SysTick_Delay_1mS(5000);
-    }  */
-
-    UART0_Println("******************************************Sending CMD0");
-
-    response = SD_Send(CMD0, Zero_Arg, dummy_CRC);
-    SysTick_Delay_1mS(100);
-
-    if(response == 0x01) UART0_Println("CMD0 Success");
-    else {
-        UART0_Println("CMD0 fail, 5s wait");
-        SysTick_Delay_1mS(5000);
-    }
-
-    UART0_Println("****************************************Sending CMD55");
-
-    response = SD_Send(CMD55, Zero_Arg, dummy_CRC);
-    SysTick_Delay_1mS(100);
-
-    if(response == 0x00) UART0_Println("CMD55 Success");
-    else {
-        UART0_Println("CMD55 fail, 5s wait");
-        SysTick_Delay_1mS(5000);
-    }
-
-    UART0_Println("****************************************Sending CMD55");
-
-    response = SD_Send(CMD55, Zero_Arg, dummy_CRC);
-    SysTick_Delay_1mS(100);
-
-    if(response == 0x00) UART0_Println("CMD55 Success");
-    else {
-        UART0_Println("CMD55 fail, 5s wait");
-        SysTick_Delay_1mS(5000);
-    }
-
-
-    UART0_Println("***************************************Sending CMD41");
-
-    response = SD_Send(CMD41, CMD41_Arg, dummy_CRC);
-    SysTick_Delay_1mS(100);
-
-    if(response == 0x00) UART0_Println("CMD41 Success");
-    else {
-        UART0_Println("CMD41 fail, 5s wait");
-        SysTick_Delay_1mS(5000);
+        UART0_Println("CMD0 failed");
+        return 0;
     }
 
 }
