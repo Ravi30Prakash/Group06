49eee69e0d0475bb6b361d102d0f342d9ae5b16b
Yadukrishnan_K_M <ee24mt013@iitdh.ac.in>
2024-11-22 19:01:59 +0530

    Adding all the fuction and variables

--- Group_06_Project_SD_card_Data_logger/definitions.h
+++ Group_06_Project_SD_card_Data_logger/definitions.h
@@ -8,25 +8,44 @@
 
 // SD Command definitions
 
-#define CMD0   0x40  // Reset command (GO_IDLE_STATE)
-#define CMD8   0x48  // Send Interface Condition command (CHECK_PATTERN)
-#define CMD58  0x49 //0x7a  // Operating Conditions Register, condition about voltage and card type, 32bit replay
-#define CMD55  0x77  // App command (prefix for other commands)
-#define CMD41  0x69  // SD Card Initialization command (SEND_OP_COND)
-#define CMD16  0x50  // Set block length (512 bytes)
+#define CMD0   0x40  // Reset command (GO_IDLE_STATE), R1 response
+#define CMD8   0x48  // Send Interface Condition command (CHECK_PATTERN), 40 bit replay, R3 response
+#define CMD55  0x77  // App command (prefix for other commands), R1 response
+#define CMD41  0x69  // SD Card Initialization command (SEND_OP_COND), R1 response
+#define CMD58  0x7a  // Operating Conditions Register, condition about voltage and card type, 40 bit replay, R3 response
+#define CMD16  0x50  // Set block length (512 bytes), R1 response
+#define CMD17  0x51  // Read single block (sector)
+#define CMD24  0x58  // Write single block (WRITE_BLOCK)
 
 // SD arguments
 
-#define Zero_Arg  0x00
+#define Zero_Arg  0x00         // for cmd0, cmd55, cmd58
 #define CMD8_Arg  0x1aa
 #define CMD41_Arg 0x40000000   // SDHC/SDXC cards: 0x40000000, SDSC cards: 0x00000000
 #define CMD16_Arg 0x200        // common block length 512 bytes
 
 // SD CRC
 
-#define CMD8_CRC  0x87
-#define dummy_CRC 0x95
+#define CMD8_CRC 0x87
+#define CMD0_CRC 0x95
+#define Zero_CRC 0x00
+#define Default_CRC 0x01
 
+//////**********************************************************/////**********************************************////
+
+// Global variables
+
+extern uint8_t response;          // 8 bit response from the SD card while sendng CMD
+extern int intia_Status;          // Initialisation status, 1- Success, 2- CMD0 fail, 3- CMD8 fail, 4- CMD55 fail, 5- CMD41 fail
+extern bool switch_1;
+extern bool switch_2;
+extern uint8_t ocr[4];            // The OCR register value
+extern uint8_t ocr_register_4B[4];
+extern unsigned long lba_begin_address,number_of_sectors,lba_addr,cluster_start,file_size,fat_begin_lba,sectors_per_fat,root_dir_first_cluster;
+extern uint8_t data_Received[512];       // Storing one sector of data from SD card, 512 bytes
+
+
+//////**********************************************************/////**********************************************////
 
 // all functions declaration
 
@@ -52,9 +71,12 @@ void modify(void);
 void SysTick_Delay_1mS(unsigned long delay);
 uint8_t SPI2_Transmit(uint8_t data);
 uint8_t SD_Send(uint8_t command, uint32_t argument, uint8_t crc);
-//uint8_t SPI_Receive(void);
-void SD_Card_Present(void);
+int SD_Card_Initialisation(void);
 void SD_Initial_Clock_Pulses(void);
+bool SD_Card_OCR_data();
+void SD_Read_Sector(uint32_t blockNo);
+bool SD_Write_Block(uint32_t blockNo, bool clear_Flag);
+bool Receive_Datablock_1B(void);
 void UART0_WriteChar(char c);
 void UART0_WriteString(const char* str);
 void UART0_Println(const char* str);
